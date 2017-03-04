633867b857490c933f3593e6f7184cf6c4a7411d
Tim Creech <tcreech@umd.edu>
2017-03-04 00:01:51 -0500

    FBSD: support kernels built with VIMAGE
    VIMAGE is FreeBSD's network virtualization support. It is not enabled by
    default as of 11-RELEASE.
    
    The main issue with VIMAGE is that without special care 'curvnet'
    returns NULL, resulting in a page fault when evaluating V_xxx macros.
    
    This support has been tested only lightly, and only for the case where
    VNET support is enabled but only one "vnet" exists.
    
    For more, see: https://wiki.freebsd.org/VIMAGE/porting-to-vimage

--- src/afs/afs_server.c
+++ src/afs/afs_server.c
@@ -42,6 +42,11 @@
 #endif
 #include <netinet/in.h>
 
+#if defined(AFS_FBSD80_ENV)
+#include <sys/param.h>
+#include <sys/jail.h>
+#endif
+
 #ifdef AFS_SGI62_ENV
 #include "h/hashing.h"
 #endif
@@ -1351,6 +1356,9 @@ afs_SetServerPrefs(struct srvAddr *const sa)
 		afsi_SetServerIPRank(sa, ifa);
     }}}
 #elif defined(AFS_FBSD_ENV)
+#if defined(AFS_FBSD80_ENV)
+    CURVNET_SET(TD_TO_VNET(curthread));
+#endif
     {
 	struct in_ifaddr *ifa;
 #if defined(AFS_FBSD80_ENV)
@@ -1388,6 +1396,9 @@ afs_SetServerPrefs(struct srvAddr *const sa)
 #endif				/* AFS_SUN5_ENV */
 #endif				/* else AFS_USERSPACE_IP_ADDR */
     sa->sa_iprank += afs_randomMod15();
+#if defined(AFS_FBSD80_ENV)
+    CURVNET_RESTORE();
+#endif
 
     return 0;
 }				/* afs_SetServerPrefs */
