dc50b492a93f4a437827f1804a7de756c0bd4e29
Tim Creech <tcreech@tcreech.com>
2017-03-05 18:15:35 -0500

    FBSD: reduce global lock frobnication in afs_root
    This change reorganizes afs_root slightly so that a temporary GLOCK drop
    is unnecessary. (It was being dropped to do a vget, but this can happen
    after the lock is normally released.)
    
    Note that it should be possible to do a vget with the GLOCK held if the
    GLOCK is sx(9)-based.
    
    Change-Id: I9712be76825a0568bb05b4e52a65c6313ca9adb6

--- src/afs/FBSD/osi_vfsops.c
+++ src/afs/FBSD/osi_vfsops.c
@@ -297,20 +297,6 @@ tryagain:
     }
     if (tvp) {
 	struct vnode *vp = AFSTOV(tvp);
-
-	ASSERT_VI_UNLOCKED(vp, "afs_root");
-	AFS_GUNLOCK();
-	error = vget(vp, LK_EXCLUSIVE | LK_RETRY, td);
-	AFS_GLOCK();
-	/* we dropped the glock, so re-check everything it had serialized */
-	if (!afs_globalVp || !(afs_globalVp->f.states & CStatd) ||
-		tvp != afs_globalVp) {
-	    vput(vp);
-	    afs_PutVCache(tvp);
-	    goto tryagain;
-	}
-	if (error != 0)
-	    goto tryagain;
 	/*
 	 * I'm uncomfortable about this.  Shouldn't this happen at a
 	 * higher level, and shouldn't we busy the top-level directory
@@ -320,11 +306,24 @@ tryagain:
 
 	afs_globalVFS = mp;
 	*vpp = vp;
+	// Drop the lock; we'll only do the vget afterward.
+	AFS_GUNLOCK();
+
+	ASSERT_VI_UNLOCKED(vp, "afs_root");
+	error = vget(vp, LK_EXCLUSIVE | LK_RETRY, td);
+	// If we had an error, don't expose the vp.
+	if (error != 0) {
+	    *vpp = NULL;
+	    error = EIO;
+	}
+    } else {
+	// How often do we get here?
+	afs_warn("afs_root has NULL tvp?\n");
+	AFS_GUNLOCK();
     }
 
     afs_Trace2(afs_iclSetp, CM_TRACE_VFSROOT, ICL_TYPE_POINTER, tvp ? AFSTOV(tvp) : NULL,
 	       ICL_TYPE_INT32, error);
-    AFS_GUNLOCK();
     crfree(cr);
     return error;
 }
