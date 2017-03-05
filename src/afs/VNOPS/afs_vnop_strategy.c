55b9a57828a76c12d69b8fdd9d077df1a4fcbce6
Tim Creech <tcreech@umd.edu>
2017-03-05 00:04:58 -0500

    FBSD: build fix for FreeBSD 11
    r285819 eliminated b_saveaddr from struct buf, while r292373 changed the
    arguments to VOP_GETPAGES. The approach used by this patch to address
    these changes was inspired by FreeBSD's nfs and samba clients.
    
    Change-Id: I3a570bbd1c1426a8e16fa339a9a4f74d3918cc7e

--- src/afs/VNOPS/afs_vnop_strategy.c
+++ src/afs/VNOPS/afs_vnop_strategy.c
@@ -99,7 +99,7 @@ int afs_ustrategy(struct buf *abp)
 #endif
 	tuio.afsio_resid = abp->b_bcount;
 #if defined(AFS_XBSD_ENV)
-	tiovec[0].iov_base = abp->b_saveaddr;
+	tiovec[0].iov_base = abp->b_data;
 #else
 	tiovec[0].iov_base = abp->b_un.b_addr;
 #endif /* AFS_XBSD_ENV */
@@ -114,7 +114,7 @@ int afs_ustrategy(struct buf *abp)
 	if (code == 0) {
 	    if (tuio.afsio_resid > 0)
 #if defined(AFS_XBSD_ENV)
-		memset(abp->b_saveaddr + abp->b_bcount - tuio.afsio_resid, 0,
+		memset(abp->b_data + abp->b_bcount - tuio.afsio_resid, 0,
 		       tuio.afsio_resid);
 #else
 		memset(abp->b_un.b_addr + abp->b_bcount - tuio.afsio_resid, 0,
@@ -176,7 +176,7 @@ int afs_ustrategy(struct buf *abp)
 #endif
 	tuio.afsio_resid = len;
 #if defined(AFS_XBSD_ENV)
-	tiovec[0].iov_base = abp->b_saveaddr;
+	tiovec[0].iov_base = abp->b_data;
 #else
 	tiovec[0].iov_base = abp->b_un.b_addr;
 #endif /* AFS_XBSD_ENV */
