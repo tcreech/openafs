14be41b91f7bf008024b412c79d3e21fa0300858
Tim Creech <tcreech@tcreech.com>
2017-03-05 18:30:07 -0500

    FBSD: remove several GLOCK drops
    With an sx(9)-based global lock, threads are allowed to sleep while
    holding it. This change removes several GLOCK drops which are now
    unnecessary as a result.
    
    Change-Id: I9d315ddf5da09fd8b8ef5ff74039049052c30394

--- src/afs/afs_osi_alloc.c
+++ src/afs/afs_osi_alloc.c
@@ -53,7 +53,7 @@ afs_osi_Alloc(size_t size)
 #ifdef AFS_LINUX20_ENV
     return osi_linux_alloc(size, 1);
 #elif defined(AFS_FBSD_ENV)
-    return osi_fbsd_alloc(size, 1);
+    return osi_fbsd_alloc(size, 0);
 #else
     return AFS_KALLOC(size);
 #endif
