146d79030b7d009c7bfef9edd8adec1d5661d73f
Tim Creech <tcreech@tcreech.com>
2017-03-05 18:31:18 -0500

    FBSD: skip vinvalbuf when paging or vnode unlocked
    Locking the vnode in afs_GetVCache is very problematic: whether we drop
    or keep the GLOCK to do it, deadlocks can occur.
    
    Nor does it seem feasible to insist that we always have a locked vp at
    this point; it may have just been created, or the calling FBSD osi code
    may not have had even known the vp yet. (E.g., when using "afs_lookup".)
    
    Furthermore, if paging is in progress, then we will certainly deadlock
    in vinvalbuf: we are the pager, so we can't wait for the pager. I have
    only seen this as a result of mmap.
    
    For now, only attempt the vinvalbuf when we already have the vp locked
    and paging is not in progress.
    
    Change-Id: I4879bb6bbede47d2f14d2c75dc196be4213e7314

--- src/afs/afs_vcache.c
+++ src/afs/afs_vcache.c
@@ -46,6 +46,11 @@
 #include "afs/afs_cbqueue.h"
 #include "afs/afs_osidnlc.h"
 
+#if defined(AFS_FBSD80_ENV)
+#include <vm/vm.h>
+#include <vm/vm_object.h>
+#endif
+
 afs_int32 afs_maxvcount = 0;	/* max number of vcache entries */
 afs_int32 afs_vcount = 0;	/* number of vcache in use now */
 
@@ -1760,14 +1765,25 @@ afs_GetVCache(struct VenusFid *afid, struct vrequest *areq,
 	if (!iheldthelock)
 	    VOP_UNLOCK(vp, LK_EXCLUSIVE, current_proc());
 #elif defined(AFS_FBSD80_ENV)
-	iheldthelock = VOP_ISLOCKED(vp);
-	if (!iheldthelock) {
-	    /* nosleep/sleep lock order reversal */
-	    vn_lock(vp, LK_EXCLUSIVE | LK_RETRY);
+    // Locking the vp here is very problematic: whether we drop or keep the
+    // GLOCK to do it, deadlocks are possible and do occur.
+    // Nor does it seem feasible to insist that we always have a locked vp at
+    // this point; it may have just been created, or the calling FBSD code may
+    // not have had even known the vp yet. (E.g., when using "afs_lookup".)
+    // For now, only do the vinvalbuf when we already have the vp locked.
+    if (VOP_ISLOCKED(vp) == LK_EXCLUSIVE) {
+	// Skip vinvalbuf if we're a pager or else we deadlock waiting for
+	// ourselves.
+	if( vp && vp->v_bufobj.bo_object &&
+		vp->v_bufobj.bo_object->paging_in_progress) {
+	    afs_warn("warning: this object has paging_in_progress=%d!\n",
+		    vp->v_bufobj.bo_object->paging_in_progress);
+	    afs_warn("info: object=%p ; vp = %p\n", vp->v_bufobj.bo_object, vp);
+	} else {
+	    // Note: we can hold our sx(9)-based GLOCK here.
+	    vinvalbuf(vp, V_SAVE, PINOD, 0); /* changed late in 8.0-CURRENT */
 	}
-	vinvalbuf(vp, V_SAVE, PINOD, 0); /* changed late in 8.0-CURRENT */
-	if (!iheldthelock)
-	    VOP_UNLOCK(vp, 0);
+    }
 #elif defined(AFS_FBSD60_ENV)
 	iheldthelock = VOP_ISLOCKED(vp, curthread);
 	if (!iheldthelock)
