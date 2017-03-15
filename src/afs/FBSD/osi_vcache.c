61e633631eec5b0aa527107f315079fcd8618452
Tim Creech <tcreech@tcreech.com>
2017-03-15 09:41:32 -0400

    FBSD: don't drop locks in osi_TryEvictVCache
    Previously, the FBSD client's osi_TryEvictVCache briefly dropped the
    GLOCK and afs_xvcache, presumably to avoid sleeping while holding the
    GLOCK. This allows races which can result in a failed afs_vop_reclaim
    after vdrop, which can lead to a panic.
    
    This change eliminates the lock drops, since the new sx-based GLOCK can
    be held by a sleeping thread. Additionally, we try to lock the vnode
    without sleeping, and report failure if unable to lock immediately. This
    avoids potential deadlock, since locking the vnode after the GLOCK is a
    lock order reversal.
    
    Change-Id: I9e20eadbd2b5578932b1928b5a5a8fc104055c31

--- src/afs/FBSD/osi_vcache.c
+++ src/afs/FBSD/osi_vcache.c
@@ -38,19 +38,20 @@ osi_TryEvictVCache(struct vcache *avc, int *slept, int defersleep)
      * This code largely copied from vfs_subr.c:vlrureclaim() */
     vholdl(vp);
 
-    ReleaseWriteLock(&afs_xvcache);
-    AFS_GUNLOCK();
-
     *slept = 1;
     /* use the interlock while locking, so no one else can DOOM this */
-    vn_lock(vp, LK_INTERLOCK|LK_EXCLUSIVE|LK_RETRY);
-    vgone(vp);
-    VOP_UNLOCK(vp, 0);
-    vdrop(vp);
+    /* We won't sleep here since this is a LOR with GLOCK. */
+    if (vn_lock(vp, LK_INTERLOCK|LK_EXCLUSIVE|LK_NOWAIT) == 0) {
+	vgone(vp);
+	VOP_UNLOCK(vp, 0);
+	vdrop(vp);
+	return 1;
+    }
 
-    AFS_GLOCK();
-    ObtainWriteLock(&afs_xvcache, 340);
-    return 1;
+    /* We failed to lock the vnode. Drop and indicate that we weren't able to
+     * flush. Note that vn_lock has dropped the interlock. */
+    vdrop(vp);
+    return 0;
 }
 
 struct vcache *
