1f90f2aba18bf324868d77163b5a1c02d9cab833
Tim Creech <tcreech@tcreech.com>
2019-08-29 21:39:57 -0400

    FBSD: follow ROOTINO->UFS_ROOTINO rename in 12
    It seems that the "ROOTINO" macro has been renamed to "UFS_ROOTINO".
    See: https://reviews.freebsd.org/D9536
    
    Change-Id: I86e6ab07154226926305e6d115aaf5e620989324

--- src/vol/devname.c
+++ src/vol/devname.c
@@ -165,7 +165,11 @@ vol_DevName(dev_t adev, char *wpath)
 	}
 #if !defined(AFS_SGI_XFS_IOPS_ENV) && !defined(AFS_LINUX22_ENV) && !defined(AFS_DARWIN_ENV)
 	if ((status.st_ino !=
+#ifdef AFS_FBSD120_ENV
+	     UFS_ROOTINO) /*|| ((status.st_mode & S_IFMT) != S_IFBLK) */ ) {
+#else
 	     ROOTINO) /*|| ((status.st_mode & S_IFMT) != S_IFBLK) */ ) {
+#endif //AFS_FBSD120_ENV
 	    continue;
 	}
 #endif
