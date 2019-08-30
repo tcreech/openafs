f149fff58f51080ba26b094cbc27a1280da17bc9
Tim Creech <tcreech@tcreech.com>
2019-08-29 21:35:36 -0400

    FBSD: accommodate 12.0's 64-bit inodes
    For more information, see: https://reviews.freebsd.org/rS318736
    
    Change-Id: I36c2bf1f35b4d1ab61a2b4d51da7514827b3551b

--- src/afs/VNOPS/afs_vnop_readdir.c
+++ src/afs/VNOPS/afs_vnop_readdir.c
@@ -159,10 +159,23 @@ struct min_direct {		/* miniature direct structure */
     uint16_t d_namlen;		/* length of string in d_name */
     uint8_t  d_type; 		/* file type, see below */
 #elif defined(AFS_DARWIN_ENV) || defined(AFS_XBSD_ENV)
+#if defined(AFS_FBSD120_ENV)
+    /* FreeBSD 12.0 moved to 64-bit inodes. */
+    ino_t d_fileno;
+    off_t d_off;
+#else
     afs_uint32 d_fileno;
+#endif
     u_short d_reclen;
     u_char d_type;
+#if defined(AFS_FBSD120_ENV)
+    /* FreeBSD 12.0 also bumped d_namlen to 16 bits. */
+    u_char  d_pad0;
+    u_short d_namlen;
+    u_short d_pad1;
+#else
     u_char d_namlen;
+#endif
 #elif defined(AFS_SUN5_ENV)
     afs_uint32 d_fileno;
     afs_int32 d_off;
