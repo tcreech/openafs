416408e096b2ce3da10fe8aacf4c8c4d84ef6a01
Andrew Deason <adeason@dson.org>
2020-04-26 00:25:10 -0500

    FBSD: Use user creds for afs_vop_putpages()
    Currently, the FBSD afs_vop_putpages uses the credentials of the
    current process for writing data back to the fileserver. Usually this
    works, but sometimes we're being called from syncer(4), which runs as
    root instead of as the user accessing the relevant file.
    
    This means that instead of using the tokens from the user that wrote
    to the relevant file, we use whatever tokens belong to uid 0. This
    usually causes an EACCES error when trying to write the data back to
    the fileserver, causing us to store EACCES (13) in avc->vc_error, and
    possibly causing a message in the kernel log like so:
    
        afs: failed to store file (0/13)
    
    Since we set vc_error during these errors, this can also cause access
    to the file to fail for the normal user process until vc_error is
    cleared (such as when the file is closed).
    
    To avoid this, store the credentials of the current user that
    successfully opens the file for writing (in avc->cred), and use those
    creds for writing back data to the fileserver. This is the same
    approach that LINUX uses as of commit 70c8deab (Use user credentials
    for Linux writepage()), and the NFS client code in FreeBSD itself (see
    the usage of n_writecred in struct nfsnode as of 12.1-RELEASE).
    
    Change-Id: I592d709b68d746bbdb326dfd7d012d6de829b905

--- src/afs/afs.h
+++ src/afs/afs.h
@@ -959,8 +959,8 @@ struct vcache {
     struct bhv_desc vc_bhv_desc;	/* vnode's behavior data. */
 #endif
 #endif				/* AFS_SGI_ENV */
-#if defined(AFS_LINUX26_ENV)
-    cred_t *cred;		/* last writer's cred */
+#if defined(AFS_LINUX26_ENV) || defined(AFS_FBSD_ENV)
+    afs_ucred_t *cred;		/* last writer's cred */
 #endif
 #ifdef AFS_LINUX24_ENV
     struct dentry *target_link; /* dentry we prefer, when we are redirecting
