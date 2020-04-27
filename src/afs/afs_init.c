6685ceea13abccef0a87f3867fb06b684899c26f
Andrew Deason <adeason@dson.org>
2020-04-27 13:28:33 -0500

    afs: Avoid touching CBRs after free
    Currently, we free our CBR structures in shutdown_vcache, but later on
    in shutdown_server, we call afs_FreeCBR on each one that's attached to
    a struct server. afs_FreeCBR doesn't actually free the memory; it just
    modifies some pointers to put the CBR on the free list. Since we do
    this after the underlying memory has been freed, it can cause a panic
    during shutdown since the structures are no longer valid.
    
    To avoid this, make the afs_FreeCBR calls inside shutdown_vcache,
    right before the memory is freed.
    
    Change-Id: I142126d6aa811762b6c234d05abdac3764dad887

--- src/afs/afs_init.c
+++ src/afs/afs_init.c
@@ -755,7 +755,6 @@ static void
 shutdown_server(void)
 {
     int i;
-    struct afs_cbr *tcbrp, *tbrp;
     struct srvAddr *sa;
 
     for (i = 0; i < NSERVERS; i++) {
@@ -772,13 +771,6 @@ shutdown_server(void)
                      * here */
                     afs_ReleaseConns(sa->conns);
 		}
-	    }
-	    for (tcbrp = ts->cbrs; tcbrp; tcbrp = tbrp) {
-		/*
-		 * Free all server's callback structs
-		 */
-		tbrp = tcbrp->next;
-		afs_FreeCBR(tcbrp);
 	    }
 	    afs_osi_Free(ts, sizeof(struct server));
 	    ts = next;
