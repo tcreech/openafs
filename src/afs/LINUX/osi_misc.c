ffac6f34c48dc5bdbaa36a0620b73b205cd28008
Andrew Deason <adeason@dson.org>
2020-05-03 10:33:28 -0500

    afs: Drop GLOCK for various Rx calls
    Most calls into Rx from libafs do so without the AFS_GLOCK, but a few
    pieces of code still hold AFS_GLOCK while making some Rx calls. A few
    calls into Rx also currently require AFS_GLOCK, but drop AFS_GLOCK
    internally, which is somewhat confusing/inconsistent. Calling Rx
    functions with AFS_GLOCK held can potentially cause locking/allocation
    problems on various platforms, such as FreeBSD where we get WITNESS
    warnings about acquiring sleepable Rx locks while holding the
    non-sleepable AFS_GLOCK.
    
    Fix a variety of Rx calls from libafs to drop AFS_GLOCK before calling
    into Rx. Specifically, this commit handles calls to rxi_GetIFInfo,
    rx_InitHost, rx_StartServer, rx_ServerProc, rx_GetConnection,
    rx_DestroyConnection/rx_PutConnection, and
    rx_SetConnSecondsUntilNatPing.
    
    For calls made via afs_start_thread, adjust afs_start_thread to accept
    a new argument that says whether to acquire AFS_GLOCK for the relevant
    function or not.
    
    For a call to rx_InitHost inside afs_InitSetup, dropping GLOCK makes
    it possible for another thread to also enter afs_InitSetup while we're
    running, before afs_InitSetup_done is set. To prevent two threads from
    running afs_InitSetup in parallel, introduce afs_InitSetup_running
    (which is set while afs_InitSetup is running), and simply wait for it
    to be cleared if it is set when we enter afs_InitSetup.
    
    This commit does not handle strictly all calls into Rx from libafs,
    since many Rx calls don't do anything interesting besides set some
    internal variables, and so AFS_GLOCK doesn't really matter for them,
    and dropping/reacquiring it around those calls may have performance
    impact.
    
    Change-Id: Ib6344f08593182ad0f812ea42eb8c91d8f082356

--- src/afs/LINUX/osi_misc.c
+++ src/afs/LINUX/osi_misc.c
@@ -160,6 +160,16 @@ afs_thread_wrapper(void *rock)
 {
     void (*proc)(void) = rock;
     __module_get(THIS_MODULE);
+    (*proc)();
+    module_put(THIS_MODULE);
+    return 0;
+}
+
+static int
+afs_thread_wrapper_glock(void *rock)
+{
+    void (*proc)(void) = rock;
+    __module_get(THIS_MODULE);
     AFS_GLOCK();
     (*proc)();
     AFS_GUNLOCK();
@@ -167,7 +177,12 @@ afs_thread_wrapper(void *rock)
     return 0;
 }
 
-void afs_start_thread(void (*proc)(void), char *name)
+void
+afs_start_thread(void (*proc)(void), char *name, int needs_glock)
 {
-    kthread_run(afs_thread_wrapper, proc, "%s", name);
+    if (needs_glock) {
+	kthread_run(afs_thread_wrapper_glock, proc, "%s", name);
+    } else {
+	kthread_run(afs_thread_wrapper, proc, "%s", name);
+    }
 }
