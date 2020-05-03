56f965b3eecfdcc9cf1065f6565ca79ba323f2e7
Andrew Deason <adeason@dson.org>
2020-05-03 10:33:28 -0500

    afs: Drop GLOCK for various Rx calls
    Most calls into Rx from libafs do so without the AFS_GLOCK, but a few
    pieces of code still hold AFS_GLOCK while making some Rx calls. A few
    calls into Rx also currently require AFS_GLOCK, but drop AFS_GLOCK
    internally, which is somewhat confusing/inconsistent. Calling Rx
    functions with AFS_GLOCK held can potentially cause locking/allocation
    problems on various platforms, such as FreeBSD where we get WITNESS
    warnings about acquiring sleepable Rx locks while holding the
    non-sleepable AFS_GLOCK.
    
    Fix a variety of Rx calls from libafs to drop AFS_GLOCK before calling
    into Rx. Specifically, this commit handles calls to rxi_GetIFInfo,
    rx_InitHost,  rx_StartServer, rx_ServerProc, rx_GetConnection,
    rx_DestroyConnection/rx_PutConnection, and
    rx_SetConnSecondsUntilNatPing.
    
    For calls made via afs_start_thread, adjust afs_start_thread to accept
    a new argument that says whether to acquire AFS_GLOCK for the relevant
    function or not.
    
    This commit does not handle strictly all calls into Rx from libafs,
    since many Rx calls don't do anything interesting besides set some
    internal variables, and so AFS_GLOCK doesn't really matter for them,
    and dropping/reacquiring it around those calls may have performance
    impact.
    
    Change-Id: Ib6344f08593182ad0f812ea42eb8c91d8f082356

--- src/afs/afs_conn.c
+++ src/afs/afs_conn.c
@@ -532,7 +532,9 @@ afs_ConnBySA(struct srvAddr *sap, unsigned short aport, afs_int32 acell,
 	 */
 	if ((service != 52) && (sap->natping == NULL)) {
 	    sap->natping = tc;
+	    AFS_GUNLOCK();
 	    rx_SetConnSecondsUntilNatPing(tc->id, 20);
+	    AFS_GLOCK();
 	}
 
 	tc->forceConnectFS = 0;	/* apparently we're appropriately connected now */
@@ -542,7 +544,9 @@ afs_ConnBySA(struct srvAddr *sap, unsigned short aport, afs_int32 acell,
     } /* end of if (tc->forceConnectFS)*/
 
     *rxconn = tc->id;
+    AFS_GUNLOCK();
     rx_GetConnection(*rxconn);
+    AFS_GLOCK();
 
     ReleaseSharedLock(&afs_xconn);
     return tc;
@@ -672,7 +676,9 @@ afs_PutConn(struct afs_conn *ac, struct rx_connection *rxconn,
 	          (unsigned long)(uintptrsz)ac, (int)ac->refCount);
     }
     ac->parent->refCount--;
+    AFS_GUNLOCK();
     rx_PutConnection(rxconn);
+    AFS_GLOCK();
 }				/*afs_PutConn */
 
 
