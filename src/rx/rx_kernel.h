7e5d4b87372b53806bf689702217d4f648e84667
Tim Creech <tcreech@tcreech.com>
2021-02-07 05:06:06 +0000

    FBSD: follow explicit epoch_tracker changes

--- src/rx/rx_kernel.h
+++ src/rx/rx_kernel.h
@@ -69,8 +69,13 @@ extern void osi_Panic(char *fmt, ...)
 #define	osi_WakeupAndYieldIfPossible(x)	    rx_Wakeup(x)
 
 #ifdef AFS_FBSD120_ENV
-# define RX_NET_EPOCH_ENTER() NET_EPOCH_ENTER()
-# define RX_NET_EPOCH_EXIT() NET_EPOCH_EXIT()
+# ifdef AFS_FBSD_EXPLICIT_EPOCH_TRACKER
+#  define RX_NET_EPOCH_ENTER() struct epoch_tracker __et; NET_EPOCH_ENTER(__et)
+#  define RX_NET_EPOCH_EXIT() NET_EPOCH_EXIT(__et)
+# else
+#  define RX_NET_EPOCH_ENTER() NET_EPOCH_ENTER()
+#  define RX_NET_EPOCH_EXIT() NET_EPOCH_EXIT()
+# endif
 #else
 # define RX_NET_EPOCH_ENTER() do { } while(0)
 # define RX_NET_EPOCH_EXIT() do { } while(0)
