d9830a1566c0b357c6a37556b070e9a59ffacd3c
Tim Creech <tcreech@tcreech.com>
2021-02-07 05:08:10 +0000

    FBSD: follow removal of VI_DOOMED

--- src/afs/FBSD/osi_vcache.c
+++ src/afs/FBSD/osi_vcache.c
@@ -30,7 +30,7 @@ osi_TryEvictVCache(struct vcache *avc, int *slept, int defersleep)
 	return evicted;
     }
 
-    if ((vp->v_iflag & VI_DOOMED) != 0) {
+    if (VN_IS_DOOMED(vp)) {
 	VI_UNLOCK(vp);
 	evicted = 1;
 	return evicted;
@@ -140,7 +140,7 @@ osi_vnhold(struct vcache *avc)
     struct vnode *vp = AFSTOV(avc);
 
     VI_LOCK(vp);
-    if ((vp->v_iflag & VI_DOOMED) != 0) {
+    if (VN_IS_DOOMED(vp)) {
 	VI_UNLOCK(vp);
 	return ENOENT;
     }
