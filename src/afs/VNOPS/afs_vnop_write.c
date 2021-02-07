39c66011f06272c3106b86cb1bcb30cec9b6a3b3
Tim Creech <tcreech@tcreech.com>
2021-02-07 05:08:10 +0000

    FBSD: follow removal of VI_DOOMED

--- src/afs/VNOPS/afs_vnop_write.c
+++ src/afs/VNOPS/afs_vnop_write.c
@@ -648,8 +648,12 @@ afs_close(OSI_VC_DECL(avc), afs_int32 aflags, afs_ucred_t *acred)
             afs_int32 opens, is_free, is_gone, is_doomed, iflag;
             struct vnode *vp = AFSTOV(avc);
             VI_LOCK(vp);
-            is_doomed =  vp->v_iflag & VI_DOOMED;
+            is_doomed =  VN_IS_DOOMED(vp);
+#if 1
+            is_free = (vp->v_holdcnt > 0);
+#else
             is_free = vp->v_iflag & VI_FREE;
+#endif
             is_gone = vp->v_iflag & VI_DOINGINACT;
             iflag = vp->v_iflag;
             VI_UNLOCK(vp);
