2ab025997b4d7f306ce3859f60247a544474497a
Tim Creech <tcreech@tcreech.com>
2023-06-11 12:07:37 -0400

    FBSD: support aarch64

--- src/config/afs_sysnames.h
+++ src/config/afs_sysnames.h
@@ -381,6 +381,8 @@
 #define SYS_NAME_ID_amd64_fbsd_131     3066
 #define SYS_NAME_ID_amd64_fbsd_132     3067
 
+#define SYS_NAME_ID_arm64_fbsd_132     3200
+
 #define SYS_NAME_ID_amd64_w2k           3400
 
 #define SYS_NAME_ID_i64_w2k             3500
