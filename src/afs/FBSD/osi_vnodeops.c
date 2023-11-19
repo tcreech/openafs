5dab2e40540d6f1affd0a8540df98de54dab7c68
Tim Creech <tcreech@tcreech.com>
2023-11-19 16:30:14 -0500

    FBSD: Add support for FBSD 14.0

--- src/afs/FBSD/osi_vnodeops.c
+++ src/afs/FBSD/osi_vnodeops.c
@@ -78,6 +78,12 @@
 # define AFS_LINK_MAX (32767)
 #endif
 
+#if __FreeBSD_version >= 1400074
+# define RELOOKUP(DVP, VPP, CNP) vfs_relookup(DVP, VPP, CNP, false)
+#else
+# define RELOOKUP(DVP, VPP, CNP) relookup(DVP, VPP, CNP)
+#endif
+
 /*
  * Here we define compatibility functions/macros for interfaces that
  * have changed between different FreeBSD versions.
@@ -278,8 +284,10 @@ afs_vop_lookup(ap)
 	if ((cnp->cn_nameiop == CREATE || cnp->cn_nameiop == RENAME)
 	    && (flags & ISLASTCN) && error == ENOENT)
 	    error = EJUSTRETURN;
+#if __FreeBSD_version < 1400068
 	if (cnp->cn_nameiop != LOOKUP && (flags & ISLASTCN))
 	    cnp->cn_flags |= SAVENAME;
+#endif
 	DROPNAME();
 	*ap->a_vpp = 0;
 	return (error);
@@ -298,8 +306,10 @@ afs_vop_lookup(ap)
     }
     *ap->a_vpp = vp;
 
+#if __FreeBSD_version < 1400068
     if (cnp->cn_nameiop != LOOKUP && (flags & ISLASTCN))
 	cnp->cn_flags |= SAVENAME;
+#endif
 
     DROPNAME();
     return error;
@@ -970,11 +980,13 @@ afs_vop_rename(ap)
 	vrele(fvp);
 	fcnp->cn_flags &= ~MODMASK;
 	fcnp->cn_flags |= LOCKPARENT | LOCKLEAF;
+#if __FreeBSD_version < 1400075
 	if ((fcnp->cn_flags & SAVESTART) == 0)
 	    panic("afs_rename: lost from startdir");
+#endif
 	fcnp->cn_nameiop = DELETE;
 	VREF(fdvp);
-	error = relookup(fdvp, &fvp, fcnp);
+	error = RELOOKUP(fdvp, &fvp, fcnp);
 	if (error == 0)
 	    vrele(fdvp);
 	if (fvp == NULL) {
--- src/config/afs_sysnames.h
+++ src/config/afs_sysnames.h
@@ -384,6 +384,7 @@
 #define SYS_NAME_ID_amd64_fbsd_141     3081
 
 #define SYS_NAME_ID_arm64_fbsd_132     3200
+#define SYS_NAME_ID_arm64_fbsd_140     3210
 
 #define SYS_NAME_ID_amd64_w2k           3400
 
