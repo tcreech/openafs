caaab6432c8e4415370fb86daec04faa1bdce8f4
Tim Creech <tcreech@tcreech.com>
2023-11-19 16:30:14 -0500

    FBSD: Add support for FBSD 14.0

--- src/config/param.arm64_fbsd_140.h
+++ src/config/param.arm64_fbsd_140.h
@@ -2,14 +2,15 @@
 #define	AFS_PARAM_H
 
 /* Machine / Operating system information */
-#define SYS_NAME	"amd64_fbsd_140"
-#define SYS_NAME_ID	SYS_NAME_ID_amd64_fbsd_140
+#define SYS_NAME	"arm64_fbsd_140"
+#define SYS_NAME_ID	SYS_NAME_ID_arm64_fbsd_140
 
 #define AFS_64BITPOINTER_ENV 1
-#define AFS_64BITUSERPOINTER_ENV 1
 
 #ifndef UKERNEL
 /* This section for kernel libafs compiles only */
+
+#define AFS_64BITUSERPOINTER_ENV 1
 
 /* Request definitions of deprecated class A/B/C networks */
 #define IN_HISTORICAL_NETS
@@ -23,26 +24,10 @@
 #define AFS_FBSD120_ENV 1
 #define AFS_FBSD121_ENV 1
 #define AFS_FBSD122_ENV 1
-#define AFS_FBSD123_ENV 1
 #define AFS_FBSD130_ENV 1
 #define AFS_FBSD131_ENV 1
 #define AFS_FBSD132_ENV 1
 #define AFS_FBSD140_ENV 1
-
-#define AFS_X86_FBSD101_ENV 1
-#define AFS_X86_FBSD102_ENV 1
-#define AFS_X86_FBSD103_ENV 1
-#define AFS_X86_FBSD104_ENV 1
-#define AFS_X86_FBSD110_ENV 1
-#define AFS_X86_FBSD111_ENV 1
-#define AFS_X86_FBSD120_ENV 1
-#define AFS_X86_FBSD121_ENV 1
-#define AFS_X86_FBSD122_ENV 1
-#define AFS_X86_FBSD123_ENV 1
-#define AFS_X86_FBSD130_ENV 1
-#define AFS_X86_FBSD131_ENV 1
-#define AFS_X86_FBSD132_ENV 1
-#define AFS_X86_FBSD140_ENV 1
 
 #else /* !defined(UKERNEL) */
 
@@ -57,7 +42,6 @@
 #define AFS_USR_FBSD120_ENV 1
 #define AFS_USR_FBSD121_ENV 1
 #define AFS_USR_FBSD122_ENV 1
-#define AFS_USR_FBSD123_ENV 1
 #define AFS_USR_FBSD130_ENV 1
 #define AFS_USR_FBSD131_ENV 1
 #define AFS_USR_FBSD132_ENV 1
