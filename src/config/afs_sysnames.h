513edb63e8e17eb15d667f3a4b14cb3c5b7bcdff
Tim Creech <tcreech@tcreech.com>
2024-06-30 11:33:51 -0400

    FBSD: Add support for FBSD 14.1
    Change-Id: I5fe4deb701594a5088f653bc6c10c5e543d3e124

--- src/config/afs_sysnames.h
+++ src/config/afs_sysnames.h
@@ -262,6 +262,7 @@
 #define SYS_NAME_ID_i386_fbsd_131      2166
 #define SYS_NAME_ID_i386_fbsd_132      2167
 #define SYS_NAME_ID_i386_fbsd_140      2170
+#define SYS_NAME_ID_i386_fbsd_141      2171
 
 #define SYS_NAME_ID_ia64_linux2		2200
 #define SYS_NAME_ID_ia64_linux22	2201
@@ -384,9 +385,11 @@
 #define SYS_NAME_ID_amd64_fbsd_131     3066
 #define SYS_NAME_ID_amd64_fbsd_132     3067
 #define SYS_NAME_ID_amd64_fbsd_140     3070
+#define SYS_NAME_ID_amd64_fbsd_141     3071
 
 #define SYS_NAME_ID_arm64_fbsd_132     3200
 #define SYS_NAME_ID_arm64_fbsd_140     3210
+#define SYS_NAME_ID_arm64_fbsd_141     3211
 
 #define SYS_NAME_ID_amd64_w2k           3400
 
